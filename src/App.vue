f8f1404c9aa05b48ec6386d8977a0ad7b94167ac
Hieu <xuanbachrtc@gmail.com>
2023-03-05 23:45:38 +0700

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,30 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <SidebarComponent />
+    <div class="main-content">
+      <NavbarComponent />
+      <router-view/>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import SidebarComponent from "./components/SideBar.vue";
+import NavbarComponent from "./components/NavBar.vue";
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "app",
+  components: { SidebarComponent, NavbarComponent },
+};
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style scoped>
+#app{
+  display: flex;
+  flex-direction: row;
+  padding: 0px;
+  margin: 0px;
+}
+.main-content{
+  width: 80vw;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,12 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { faUserSecret, faBars, faChevronRight, faList, faBoxArchive, faLayerGroup, faArrowsRotate, faArrowDown, faPlus, faFileImport, faLink } from '@fortawesome/free-solid-svg-icons'
+import router from './router'
+library.add(faUserSecret, faBars, faChevronRight, faList, faBoxArchive, faLayerGroup, faArrowsRotate, faArrowDown, faPlus, faFileImport, faLink)
 
-createApp(App).mount('#app')
+const app = createApp(App)
+.component('font-awesome-icon', FontAwesomeIcon)
+app.use(router)
+app.mount('#app');
