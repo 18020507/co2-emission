0fbb9ef53ba86062b17c065a97ee700da4b71837
Hieu <xuanbachrtc@gmail.com>
2023-03-06 23:52:02 +0700

    new code for account set up UI

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,12 @@
 import { createRouter, createWebHistory } from "vue-router";
 import DashboardView from "@/views/dashboard/Dashboard.vue";
 import HeavyTruckView from "@/views/heavy-truck/HeavyTruckView.vue";
+import AccountSetupView from "@/views/account-setup/AccountSetupView.vue"
 
 const routes = [
   { path: "/heavy-truck", component: HeavyTruckView },
   { path: "/dashboard", component: DashboardView },
+  { path: "/account-setup", component: AccountSetupView },
 ];
 
 const router = createRouter({
---
+++ src/views/account-setup/components/SelectIndustryService.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="container-wrap">
+    <div class="name-form-child">{{ nameFormChild }}</div>
+    <div class="form-content"></div>
+  </div>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+import VSelect from "../../components/VSelect.vue";
+
+export default defineComponent({
+  components: VSelect,
+  props: {
+    nameFormChild: {
+      type: String,
+      default: "",
+    },
+  },
+});
+</script>
+
+<style scoped></style>
